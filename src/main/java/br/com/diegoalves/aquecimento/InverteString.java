93c6eb5e8c6350f01e1d9118234bfd7ff4b819a8
Diego Alves <diegohalves.dev@gmail.com>
2026-05-01 20:12:42 -0400

    Invertendo String

--- src/main/java/br/com/diegoalves/aquecimento/InverteString.java
+++ src/main/java/br/com/diegoalves/aquecimento/InverteString.java
@@ -1,4 +1,18 @@
 package br.com.diegoalves.aquecimento;
 
+import java.util.Scanner;
+
 public class InverteString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("============InverteString============");
+        System.out.print("Por favor, informe uma palavra: ");
+        String palavra = sc.nextLine();
+        StringBuilder palavraInvertida = new StringBuilder();
+        for (int i = palavra.length(); i > 0; i--) {
+            palavraInvertida.append(palavra.charAt(i - 1));
+        }
+        System.out.println(palavraInvertida);
+
+    }
 }
